a44e4724eb2da678cb65a39187b8d55bd6e415f6
JPham8010 <jpham8010@gmail.com>
2020-02-08 16:11:28 -0600

    descriptions for question structures

--- app/src/main/java/Topics.java
+++ app/src/main/java/Topics.java
@@ -1,6 +1,45 @@
+import java.util.ArrayList;
+
 public class Topics
 {
-     private ArrayList<String> questions;
+    //Includes: Abortion, First Amendment, Death Penalty,
+    ArrayList<String> socialIssuesQuestions;
+
+    //Includes: Gun control, Net neutrality, Drug policy, Social Media Regulation, Social Security,
+    ArrayList<String> domesticPolicyIssuesQuestions;
+
+    //Includes: Equal pay, Taxes, Corporate tax, Government spending, Unions, Domestic jobs,
+    ArrayList<String> economicIssuesQuestions;
+
+    //Includes: Climate change, Alternative energy sources, Fracking (oil), Plastic (materials), Companies and the environment
+    ArrayList<String> EnvironmentalIssuesQuestions;
+
+    //Includes: Borders, Immigraiton policies - harsh or naw?, Border security, Illegal immigrants
+    ArrayList<String> immigrationIssuesQuestions;
+
+    //Includes: Drug regulation prices, mental health, Affordable care act, WEEEEEED,
+    ArrayList<String> healthcareIssuesQuestions;
+
+    //Includes: Student loans/debt, Free college for all, Common core/other things for primary/secondary schools
+    ArrayList<String> EducationIssuesQuestions;
+
+    //Includes: Electoral college, Voter fraud, minimum voting age, criminal politians
+    ArrayList<String> electoralIssuesQuestions;
+
+    //Includes: Vaccines, Nuclear energy, space
+    ArrayList<String> scienceIssuesQuestions;
+
+    //Includes: military, foreign assassination
+    ArrayList<String> nationalSecurityQuestions;
+
+    //Includes: United Nations, Middle East, Refugees, military spending, war
+    ArrayList<String> foriegnPolicyIssuesQuestions;
+
+    //Includes: private prisons, prison overcrowding, criminal voting rights, police brutality
+    ArrayList<String> criminalIssuesQuestions;
+
+    //public transpotation
+    ArrayList<String> transportationIssuesQuestions;
 
 
 }
